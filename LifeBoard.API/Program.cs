0999a961d2733bd3ae08e3fab94c2a263616b06a
KelisGates22 <kelisgates@yahoo.com>
2026-06-29 12:21:35 -0400

    Add auto migration on startup for production deployment

--- LifeBoard.API/Program.cs
+++ LifeBoard.API/Program.cs
@@ -85,4 +85,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors("AllowFrontend");
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
